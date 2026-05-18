c721a76a4f9f99da99dca541a6ac0b11f82b5dd0
Cleveland Sullivan <salanosullivan@gmail.com>
2026-05-18 05:16:16 +0200

    Add customer and polices views to model

---
+++ models/gold/vw_customers.sql
@@ -0,0 +1,25 @@
+{{
+  config(
+        materialized = "view",
+        schema='gold'
+    )
+}}
+
+WITH base_query AS (
+  /*---------------------------------------------------------------------------
+        1) Base Query: Retrieves core columns from gold.claims, gold.customers and gold.policies
+        ---------------------------------------------------------------------------*/
+  SELECT
+    cu.[cust_id],
+    cu.[Date_of_Birth],
+    cu.[Gender],
+    cu.[Contact_Number],
+    cu.[CustomerName]
+  FROM
+    {{ ref('st_ins_customers') }} AS cu
+  WHERE
+    cu.[dbt_valid_to] IS NULL
+)
+SELECT *
+FROM
+  base_query
---
+++ models/gold/vw_policies.sql
@@ -0,0 +1,31 @@
+{{
+  config(
+        materialized = "view",
+        schema='gold'
+    )
+}}
+
+WITH base_query AS (
+  /*---------------------------------------------------------------------------
+        1) Base Query: Retrieves core columns from silver.policies
+        ---------------------------------------------------------------------------*/
+  SELECT
+    p.[cust_id],
+    p.[policy_id],
+    p.[policy_type],
+    p.[status] AS [policy_status],
+    p.[start_date],
+    p.[end_date],
+    p.[coverage_amount],
+    p.[annual_premium],
+    p.[monthly_payment],
+    p.[distribution_channel]
+  FROM
+    {{ ref('st_ins_policies') }} AS p
+  WHERE
+    p.[dbt_valid_to] IS NULL
+
+)
+SELECT *
+FROM
+  base_query
