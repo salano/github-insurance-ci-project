c9e64ccdb9989d7fe4ae53a0362b6f1d6cdd3ce3
Cleveland Sullivan <salanosullivan@gmail.com>
2026-05-28 21:34:11 +0200

    updated customer view to meet sqlfluff aliasing rules

--- models/gold/vw_customers.sql
+++ models/gold/vw_customers.sql
@@ -16,7 +16,7 @@ WITH base_query AS (
     cu.[Contact_Number],
     cu.[CustomerName]
   FROM
-    {{ ref('st_ins_customers') }} cu -- noqa: AL01
+    {{ ref('st_ins_customers') }} cu
   WHERE
     cu.[dbt_valid_to] IS NULL
 )
